c48406277aec76f091f290572485036f791182cd
tcum17 <habcummings@gmail.com>
2022-03-05 18:43:53 -0600

    3 files modified, 7 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
+import BlogForm from './components/BlogForm';
+import RemovePost from './components/RemovePost';
+import posts from './components/Posts';
+import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 
 export default function App() {
+  const [users, setUsers] = useState(posts);
+
+  const submitPost = (user) => {
+    const num = Math.floor(Math.random() * 10000) + 1;
+    const newUser = { num, ...user };
+    setUsers([...users, newUser]);
+  };
+
+  const deletePost = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div class="container">
+      <div class="row">
+        <BlogForm onAdd={submitPost} />
+        {users.length > 0 ? (
+          <RemovePost users={users} onDelete={deletePost} />
+        ) : (
+          'There are currently no posts'
+        )}
+      </div>
     </div>
   );
 }
